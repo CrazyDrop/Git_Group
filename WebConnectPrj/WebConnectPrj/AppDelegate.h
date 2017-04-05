ef5e0224e0bc7a7cc61db4fe7f5695cef4583739
Apple <erik.zhang@zhongan.com>
2017-04-05 11:26:19 +0800

    historyUpdate
    historyUpdate

--- WebConnectPrj/WebConnectPrj/AppDelegate.h
+++ WebConnectPrj/WebConnectPrj/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "BaseDataModel.h"
-
+//update AppDelegate
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
